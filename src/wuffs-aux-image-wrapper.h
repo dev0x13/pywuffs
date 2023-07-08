511ab0d39de701854000ea055a010c8875488848
Georgiy Manuilov <george.manuilov@gmail.com>
2023-07-08 20:38:42 +0200

    Add wrapper API for wuffs_aux image decoding API

---
+++ src/wuffs-aux-image-wrapper.h
@@ -0,0 +1,299 @@
+#pragma once
+
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// This API wraps the wuffs_aux API for image decoding. The wrapper is needed
+// since the wuffs_aux API uses the callback-based approach which doesn't play
+// well with Python-C++ interop.
+// Although the API is different from wuffs_aux, it's supposed to be as thin as
+// possible including minor aspect like the default non-configured behavior.
+
+namespace wuffs_aux_wrap {
+
+enum class ImageDecoderFlags : uint64_t {
+#define IDFE(flag)         \
+  REPORT_METADATA_##flag = \
+      wuffs_aux::DecodeImageArgFlags::REPORT_METADATA_##flag
+  IDFE(BGCL),
+  IDFE(CHRM),
+  IDFE(EXIF),
+  IDFE(GAMA),
+  IDFE(ICCP),
+  IDFE(KVP),
+  IDFE(MTIM),
+  IDFE(OFS2),
+  IDFE(PHYD),
+  IDFE(SRGB),
+  IDFE(XMP)
+#undef IDF_ENTRY
+};
+
+enum class ImageDecoderQuirks : uint32_t {
+  IGNORE_CHECKSUM = WUFFS_BASE__QUIRK_IGNORE_CHECKSUM,
+  GIF_DELAY_NUM_DECODED_FRAMES = WUFFS_GIF__QUIRK_DELAY_NUM_DECODED_FRAMES,
+  GIF_FIRST_FRAME_LOCAL_PALETTE_MEANS_BLACK_BACKGROUND =
+      WUFFS_GIF__QUIRK_FIRST_FRAME_LOCAL_PALETTE_MEANS_BLACK_BACKGROUND,
+  GIF_QUIRK_HONOR_BACKGROUND_COLOR = WUFFS_GIF__QUIRK_HONOR_BACKGROUND_COLOR,
+  GIF_IGNORE_TOO_MUCH_PIXEL_DATA = WUFFS_GIF__QUIRK_IGNORE_TOO_MUCH_PIXEL_DATA,
+  GIF_IMAGE_BOUNDS_ARE_STRICT = WUFFS_GIF__QUIRK_IMAGE_BOUNDS_ARE_STRICT,
+  GIF_REJECT_EMPTY_FRAME = WUFFS_GIF__QUIRK_REJECT_EMPTY_FRAME,
+  GIF_REJECT_EMPTY_PALETTE = WUFFS_GIF__QUIRK_REJECT_EMPTY_PALETTE
+};
+
+enum class ImageDecoderType : uint32_t {
+#define IDTE(dt) dt = WUFFS_BASE__FOURCC__##dt
+  IDTE(BMP),
+  IDTE(GIF),
+  IDTE(NIE),
+  IDTE(PNG),
+  IDTE(TGA),
+  IDTE(WBMP),
+  IDTE(JPEG)
+#undef IDTE
+};
+
+enum class PixelFormat : uint32_t {
+#define PFE(pf) pf = WUFFS_BASE__PIXEL_FORMAT__##pf
+  PFE(A),
+  PFE(Y),
+  PFE(Y_16LE),
+  PFE(Y_16BE),
+  PFE(YA_NONPREMUL),
+  PFE(YA_PREMUL),
+  PFE(YCBCR),
+  PFE(YCBCRA_NONPREMUL),
+  PFE(YCBCRK),
+  PFE(YCOCG),
+  PFE(YCOCGA_NONPREMUL),
+  PFE(YCOCGK),
+  PFE(INDEXED__BGRA_NONPREMUL),
+  PFE(INDEXED__BGRA_PREMUL),
+  PFE(INDEXED__BGRA_BINARY),
+  PFE(BGR_565),
+  PFE(BGR),
+  PFE(BGRA_NONPREMUL),
+  PFE(BGRA_NONPREMUL_4X16LE),
+  PFE(BGRA_PREMUL),
+  PFE(BGRA_PREMUL_4X16LE),
+  PFE(BGRA_BINARY),
+  PFE(BGRX),
+  PFE(RGB),
+  PFE(RGBA_NONPREMUL),
+  PFE(RGBA_NONPREMUL_4X16LE),
+  PFE(RGBA_PREMUL),
+  PFE(RGBA_PREMUL_4X16LE),
+  PFE(RGBA_BINARY),
+  PFE(RGBX),
+  PFE(CMY),
+  PFE(CMYK)
+#undef PFE
+};
+
+enum class PixelBlend : uint32_t {
+  SRC = WUFFS_BASE__PIXEL_BLEND__SRC,
+  SRC_OVER = WUFFS_BASE__PIXEL_BLEND__SRC_OVER
+};
+
+// This struct hosts wuffs_aux::DecodeImage arguments in more user- and
+// Python- friendly fashion
+struct ImageDecoderConfig {
+  std::vector<ImageDecoderFlags> flags;
+  PixelBlend pixel_blend = static_cast<PixelBlend>(
+      wuffs_aux::DecodeImageArgPixelBlend::DefaultValue().repr);
+  std::vector<ImageDecoderQuirks> quirks;
+  uint32_t background_color =
+      wuffs_aux::DecodeImageArgBackgroundColor::DefaultValue().repr;
+  uint32_t max_incl_dimension =
+      wuffs_aux::DecodeImageArgMaxInclDimension::DefaultValue().repr;
+  uint64_t max_incl_metadata_length =
+      wuffs_aux::DecodeImageArgMaxInclMetadataLength::DefaultValue().repr;
+  std::vector<ImageDecoderType> enabled_decoders = {
+      ImageDecoderType::BMP, ImageDecoderType::GIF, ImageDecoderType::NIE,
+      ImageDecoderType::PNG, ImageDecoderType::TGA, ImageDecoderType::WBMP};
+  uint32_t pixel_format = wuffs_base__make_pixel_format(
+                              static_cast<uint32_t>(PixelFormat::BGRA_PREMUL))
+                              .repr;
+};
+
+// This structs represents the wuffs_aux::DecodeImageCallbacks::HandleMetadata
+// input
+struct MetadataEntry {
+  wuffs_base__more_information minfo{};
+  std::vector<uint8_t> data;
+
+  MetadataEntry(const wuffs_base__more_information& minfo,
+                std::vector<uint8_t>&& data)
+      : minfo(minfo), data(std::move(data)) {}
+
+  MetadataEntry() : minfo(wuffs_base__empty_more_information()) {}
+
+  MetadataEntry(MetadataEntry&& other) noexcept {
+    minfo = other.minfo;
+    std::swap(data, other.data);
+  }
+
+  MetadataEntry& operator=(MetadataEntry&& other) noexcept {
+    if (this != &other) {
+      minfo = other.minfo;
+      std::swap(data, other.data);
+    }
+    return *this;
+  }
+
+  MetadataEntry(const wuffs_aux_wrap::MetadataEntry& other) = default;
+
+  MetadataEntry& operator=(const wuffs_aux_wrap::MetadataEntry& other) =
+      default;
+};
+
+struct ImageDecodingResult {
+  std::vector<uint8_t> decoded_data;
+  std::vector<MetadataEntry> reported_metadata;
+  std::string error_message;
+
+  ImageDecodingResult() = default;
+
+  ImageDecodingResult(ImageDecodingResult&& other) noexcept {
+    std::swap(decoded_data, other.decoded_data);
+    std::swap(reported_metadata, other.reported_metadata);
+    std::swap(error_message, other.error_message);
+  }
+
+  ImageDecodingResult& operator=(ImageDecodingResult&& other) noexcept {
+    if (this != &other) {
+      std::swap(decoded_data, other.decoded_data);
+      std::swap(reported_metadata, other.reported_metadata);
+      std::swap(error_message, other.error_message);
+    }
+    return *this;
+  }
+};
+
+class ImageDecoder : public wuffs_aux::DecodeImageCallbacks {
+ public:
+  explicit ImageDecoder(const ImageDecoderConfig& config)
+      : quirks_vector_({config.quirks.begin(), config.quirks.end()}),
+        enabled_decoders_(
+            {config.enabled_decoders.begin(), config.enabled_decoders.end()}),
+        pixel_format_(wuffs_base__make_pixel_format(config.pixel_format)),
+        quirks_(wuffs_aux::DecodeImageArgQuirks(
+            reinterpret_cast<uint32_t*>(quirks_vector_.data()),
+            quirks_vector_.size())),
+        flags_(GetFlagsBitmask(config.flags)),
+        pixel_blend_(wuffs_aux::DecodeImageArgPixelBlend(
+            static_cast<uint32_t>(config.pixel_blend))),
+        background_color_(
+            wuffs_aux::DecodeImageArgBackgroundColor(config.background_color)),
+        max_incl_dimension_(wuffs_aux::DecodeImageArgMaxInclDimension(
+            config.max_incl_dimension)),
+        max_incl_metadata_length_(
+            wuffs_aux::DecodeImageArgMaxInclMetadataLength(
+                config.max_incl_metadata_length)) {}
+
+  /* DecodeImageCallbacks methods implementation */
+
+  wuffs_base__image_decoder::unique_ptr SelectDecoder(
+      uint32_t fourcc, wuffs_base__slice_u8 prefix_data,
+      bool prefix_closed) override {
+    if (enabled_decoders_.count(static_cast<ImageDecoderType>(fourcc)) == 0) {
+      return {nullptr, &free};
+    }
+    return wuffs_aux::DecodeImageCallbacks::SelectDecoder(fourcc, prefix_data,
+                                                          prefix_closed);
+  }
+
+  std::string HandleMetadata(const wuffs_base__more_information& minfo,
+                             wuffs_base__slice_u8 raw) override {
+    decoding_result_.reported_metadata.push_back(
+        {minfo, {raw.ptr, raw.ptr + raw.len}});
+    return "";
+  }
+
+  wuffs_base__pixel_format SelectPixfmt(
+      const wuffs_base__image_config&) override {
+    return pixel_format_;
+  }
+
+  // This implementation is essentially the same as the default one except that
+  // it uses the "decoding_result_" field for allocating output buffer
+  AllocPixbufResult AllocPixbuf(const wuffs_base__image_config& image_config,
+                                bool allow_uninitialized_memory) override {
+    uint32_t w = image_config.pixcfg.width();
+    uint32_t h = image_config.pixcfg.height();
+    if ((w == 0) || (h == 0)) {
+      return {""};
+    }
+    uint64_t len = image_config.pixcfg.pixbuf_len();
+    if (len == 0 || SIZE_MAX < len) {
+      return {wuffs_aux::DecodeImage_UnsupportedPixelConfiguration};
+    }
+    if (allow_uninitialized_memory) {
+      decoding_result_.decoded_data.resize(len);
+    } else {
+      decoding_result_.decoded_data.resize(len, 0);
+    }
+    wuffs_base__pixel_buffer pixbuf;
+    wuffs_base__status status = pixbuf.set_from_slice(
+        &image_config.pixcfg,
+        wuffs_base__make_slice_u8(decoding_result_.decoded_data.data(),
+                                  decoding_result_.decoded_data.size()));
+    if (!status.is_ok()) {
+      decoding_result_.decoded_data.clear();
+      return {status.message()};
+    }
+    return {wuffs_aux::MemOwner(nullptr, &free), pixbuf};
+  }
+
+  /* End of DecodeImageCallbacks methods implementation */
+
+  ImageDecodingResult Decode(const uint8_t* data, size_t size) {
+    wuffs_aux::sync_io::MemoryInput input(data, size);
+    return DecodeInternal(input);
+  }
+
+  ImageDecodingResult Decode(const std::string& path_to_file) {
+    FILE* f = fopen(path_to_file.c_str(), "r");
+    wuffs_aux::sync_io::FileInput input(f);
+    ImageDecodingResult result = DecodeInternal(input);
+    fclose(f);
+    return result;
+  }
+
+ private:
+  static uint64_t GetFlagsBitmask(const std::vector<ImageDecoderFlags>& flags) {
+    uint64_t bitmask = 0;
+    for (const auto f : flags) {
+      bitmask |= static_cast<uint64_t>(f);
+    }
+    return bitmask;
+  }
+
+  ImageDecodingResult DecodeInternal(wuffs_aux::sync_io::Input& input) {
+    wuffs_aux::DecodeImageResult decode_image_result = wuffs_aux::DecodeImage(
+        *this, input, quirks_, flags_, pixel_blend_, background_color_,
+        max_incl_dimension_, max_incl_metadata_length_);
+    decoding_result_.error_message =
+        std::move(decode_image_result.error_message);
+    if (!decode_image_result.pixbuf.pixcfg.is_valid()) {
+      decoding_result_.decoded_data.clear();
+    }
+    return std::move(decoding_result_);
+  }
+
+ private:
+  ImageDecodingResult decoding_result_;
+  std::vector<ImageDecoderQuirks> quirks_vector_;
+  std::unordered_set<ImageDecoderType> enabled_decoders_;
+  wuffs_base__pixel_format pixel_format_;
+  wuffs_aux::DecodeImageArgQuirks quirks_;
+  wuffs_aux::DecodeImageArgFlags flags_;
+  wuffs_aux::DecodeImageArgPixelBlend pixel_blend_;
+  wuffs_aux::DecodeImageArgBackgroundColor background_color_;
+  wuffs_aux::DecodeImageArgMaxInclDimension max_incl_dimension_;
+  wuffs_aux::DecodeImageArgMaxInclMetadataLength max_incl_metadata_length_;
+};
+
+}  // namespace wuffs_aux_wrap
