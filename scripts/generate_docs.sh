b4a115cd310a2b61f7ecf300235179e83e09f8ce
Georgiy Manuilov <george.manuilov@gmail.com>
2023-07-13 12:56:05 +0200

    Replaced self-hosted docs with Read The Docs

--- scripts/generate_docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-python3 -m pip install sphinx sphinx_epytext sphinx-rtd-theme
-
-python3 -m sphinx -b html -E -a "$script_dir/../docs/src/" "$script_dir/../docs/"
