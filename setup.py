b7b2d901982cb8946b15664ab793d7179dd0a251
Georgiy Manuilov <george.manuilov@gmail.com>
2023-10-08 17:49:53 +0200

    [skip ci] Fix Python package

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ ext_modules = [
 ]
 
 setup(name="pywuffs",
-      version="1.1.0",
+      version="1.1.1",
       description="Python bindings for Wuffs the Library",
       author="Georgiy Manuilov",
       url="https://github.com/dev0x13/pywuffs",
